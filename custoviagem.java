0a83ba6a8c0398b62e62dabd91f4912784074629
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-02-08 21:52:15 -0300

    08/02/23

---
+++ custoviagem.java
@@ -0,0 +1,17 @@
+/**
+ * custoviagem
+ */
+public class custoviagem {
+
+    public static void main(String[] args) {
+        int distanciaPercorrida = 148; // em km
+        double consumoTransporte = 10; // em km/l
+        double precoCombustivel = 4.99; // em R$
+        double quantidadeLitrosCombustivel = distanciaPercorrida/consumoTransporte;
+        double custoTotal = quantidadeLitrosCombustivel*precoCombustivel;
+            System.out.println("O Custo Total da Viagem Foi");
+            System.out.print("R$"+CustoTotal);}      
+    }
+}
+ * custoviagem
+ */
