8a8dd62c8236d28bf22ab7b2da431b41a2d26c56
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-02-23 21:58:58 -0300

    23/02/23

--- EstruturaDecisaoif/estruturadecisaoIf.java
+++ EstruturaDecisaoif/estruturadecisaoIf.java
@@ -17,8 +17,11 @@ public class estruturadecisaoIf {
         if(precoProduto >=300 && precoProduto<400){
             percentualDescontos =15.0;
         }
-        if (precoProduto >=400){
+        if (precoProduto >=400 && precoProduto<500){
             percentualDescontos =20.0;
+        }
+        if (precoProduto >=500){
+            percentualDescontos = 25.0;
         }
 
         // Regra de três
@@ -26,5 +29,30 @@ public class estruturadecisaoIf {
         double precoComDesconto = precoProduto -desconto;
         System.out.println("O produto saira por R$" + precoComDesconto);
         
+    }
+    public void decisaoIfElse(){
+        System.out.println("Degite o preço do Produto");
+        double precoProduto = sc.nextDouble();
+        double percentualDescontos = 0.0;
+        if(precoProduto<100){
+         percentualDescontos = 0.0;
+        } else if(precoProduto >= 100 
+        && precoProduto<200)
+        {
+            percentualDescontos = 5.0;
+        } else if (precoProduto <=200
+        && precoProduto<300)
+        { 
+            percentualDescontos =10.0;
+
+        } else {
+           percentualDescontos = 15.0; 
+        }
+        
+
+        // Regra de três
+        double desconto = (precoProduto * percentualDescontos) / 100;
+        double precoComDesconto = precoProduto -desconto;
+        System.out.println("O produto saira por R$" + precoComDesconto);
     }
 }
