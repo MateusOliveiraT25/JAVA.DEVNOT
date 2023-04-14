c859f61d181af6cf4a58d2812c36a0b6a02de2b8
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-04-13 21:43:34 -0300

    13/04

---
+++ ExerciciosWhile/app.java
@@ -0,0 +1,9 @@
+package ExerciciosWhile;
+
+public class app {
+  
+        public static void main(String[] args) {
+            exercicioWhile obj1 = new  exercicioWhile();
+            obj1.exercicio2(); 
+        }
+}
