8ca93e8fc9705cf1f24b0bf39582c116bb5acf43
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-04-18 21:59:29 -0300

    18/04/23

---
+++ InteracaoFor/App.java
@@ -0,0 +1,8 @@
+package InteracaoFor;
+
+public class App {
+    public static void main(String[] args) {
+        ExerciciosFor obj1 = new   ExerciciosFor();
+        obj1.exercicio4ex(); 
+    }
+}
--- InteracaoWhile/App.java
+++ InteracaoWhile/App.java
@@ -2,7 +2,7 @@ package InteracaoWhile;
 
 public class App {
     public static void main(String[] args) {
-        ExerciciosWhile obj1 = new  ExerciciosWhile();
-        obj1.exercicio5(); 
+        ExerciciosWhile  obj1 = new  ExerciciosWhile ();
+        obj1.exemplo1(); 
     }
 }
