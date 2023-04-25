e23db6ed457109089169ae011d3544207fdad742
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-04-25 23:08:57 +0000

    25/04/23

--- InteracaoWhile/App.java
+++ InteracaoWhile/App.java
@@ -3,6 +3,6 @@ package InteracaoWhile;
 public class App {
     public static void main(String[] args) {
         ExemploWhile obj1 = new ExemploWhile();
-        obj1.exemplo3();
+        obj1.exemplo1();
     }
 }
