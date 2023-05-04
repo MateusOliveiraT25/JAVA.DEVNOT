b36ac50abf91db7a72968f2d843e975718fbbedb
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-05-04 19:04:07 -0300

    Add files via upload

---
+++ Java/InteracaoWhile/App.java
@@ -0,0 +1,8 @@
+package InteracaoWhile;
+
+public class App {
+    public static void main(String[] args) {
+        ExerciciosWhile  obj1 = new  ExerciciosWhile ();
+        obj1.exemplo1(); 
+    }
+}
